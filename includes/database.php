51828aff4197b89b7f8144601835ef97790a3a67
Ricardo Yokoya <101286776+ricardo-yokoya@users.noreply.github.com>
2026-08-13 07:13:08 +0000

    update database

--- includes/database.php
+++ includes/database.php
@@ -2,7 +2,7 @@
 $username = "progearhub";
 $password = "password";
 // create connection
-$connection = mysqli_connect("db",$username,$password, "progearhub");
+$connection = mysqli_connect("db",$username,$password, "ProGearHub");
 // check if connection is successful
 if (!$connection) {
     echo "<h1 style='color:red;'>Database connection error!</h1>";
